e0d5987760a9e126f2a9c3493c3f735194556658
rajgaria7 <156538651+rajgaria7@users.noreply.github.com>
2024-07-21 18:11:15 -0600

    added basic design

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ## JOKES By Chuck Norris
+
+
+ - Author - Abhishek Rajgaria
+ - Github repo: https://github.com/abhishekrajgaria/JokesApp
+ - Project Summary: App will fetch jokes from https://api.chucknorris.io/jokes/random and will store it using the Room Database.
+ - Features: Button to fetch a joke, display full list of jokes and Database support so that users don't lose old jokes when they close and reopen the app.
+
+### Project Structure
+
+### Resources
--- app/src/main/java/com/example/jokesapp/MainActivity.kt
+++ app/src/main/java/com/example/jokesapp/MainActivity.kt
@@ -1,47 +1,131 @@
 package com.example.jokesapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jokesapp.ui.theme.JokesAppTheme
+
+
+/*
+Author - Abhishek Rajgaria
+ */
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JokesAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Abhishek",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+//            JokesAppTheme {
+                JokeApp()
+//            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    JokesAppTheme {
-        Greeting("Android")
+fun JokeApp(){
+
+    val jokes:List<String> = listOf("Joke 1: Hahaha", "Joke 2: Nananan", "Joke 3: Jajaja")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        text = "Chuck Norris Jokes"
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.height(48.dp)
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "@Copyright 2024"
+                )
+            }
+        }
+    ){ innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(1.dp)
+            ) {
+
+            Button(
+                onClick = {}, //TODO:
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text("Get Joke")
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            LazyColumn(
+                modifier= Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(jokes) {joke ->
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.secondaryContainer
+                        )
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            textAlign = TextAlign.Left,
+                            text = joke
+                        )
+                    }
+                }
+            }
+        }
     }
 }
